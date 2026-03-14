a73188d40f1f924519e3cb2fbb26ef7f6e891d80
Heily Rios <heilyy.riosa@autonoma.edu.co>
2026-03-13 21:12:22 -0500

    feat(auth): endpoint de login con autenticación JWT

--- config/auth.php
+++ config/auth.php
@@ -16,6 +16,7 @@ return [
     'defaults' => [
         'guard' => 'web',
         'passwords' => 'users',
+        
     ],
 
     /*
@@ -34,12 +35,18 @@ return [
     | Supported: "session"
     |
     */
-
     'guards' => [
+
         'web' => [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
+        ],
+
     ],
 
     /*
