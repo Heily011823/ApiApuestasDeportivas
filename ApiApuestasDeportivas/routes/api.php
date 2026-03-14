063b8a5ef7c7adf79038b4b8da81414d4da7273d
Maria Paz Puerta Acevedo <mariap.puertaa@autonoma.edu.co>
2026-03-13 23:40:59 -0500

    refactor(eventos): ajustar EventController a la API y mantener mensajes completos

--- ApiApuestasDeportivas/routes/api.php
+++ ApiApuestasDeportivas/routes/api.php
@@ -24,25 +24,30 @@ Route::middleware('auth:api')->group(function () {
     // Usuario autenticado
     Route::get('me', [AuthController::class, 'me']);
 
-    // Ver eventos
-    Route::get('eventos', [EventoController::class, 'index'])
+    // Eventos
+
+    // Usuario logueado
+    Route::get('me', [AuthController::class, 'me']);
+
+    // Listar eventos
+    Route::get('eventos', [EventController::class, 'index'])
         ->middleware('role:admin,usuario');
 
     // Ver evento por id
-    Route::get('eventos/{id}', [EventoController::class, 'show'])
+    Route::get('eventos/{id}', [EventController::class, 'show'])
         ->middleware('role:admin,usuario');
 
     // Crear evento
-    Route::post('eventos', [EventoController::class, 'store'])
+    Route::post('eventos', [EventController::class, 'store'])
         ->middleware('role:admin');
 
     // Actualizar evento
-    Route::put('eventos/{id}', [EventoController::class, 'update'])
+    Route::put('eventos/{id}', [EventController::class, 'update'])
         ->middleware('role:admin');
 
     // Eliminar evento
-    Route::delete('eventos/{id}', [EventoController::class, 'destroy'])
+    Route::delete('eventos/{id}', [EventController::class, 'destroy'])
         ->middleware('role:admin');
-
+        
 });
 
